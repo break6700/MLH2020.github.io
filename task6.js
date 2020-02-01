260d91854e7ff0e8bd8b6b794a9a711eb07f7331
Tass <45282891+Tasse007@users.noreply.github.com>
2020-02-01 21:50:27 +0000

    Create task6.js

---
+++ task6.js
@@ -0,0 +1,35 @@
+function init()
+{
+    var map = L.map ("userMap");
+
+    var attrib="Map data copyright OpenStreetMap contributors, Open Database Licence";
+
+    L.tileLayer
+        ("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            { attribution: attrib } ).addTo(map);
+            
+    map.setView([50.907435, -1.404244], 14);
+	
+	if(navigator.geolocation)
+    {
+        navigator.geolocation.watchPosition (
+
+            gpspos=> {
+                console.log(`Lat ${gpspos.coords.latitude} Lon ${gpspos.coords.longitude}`); // show in console for testing purposes
+				map.setView([gpspos.coords.latitude, gpspos.coords.longitude], 14);
+            },
+
+            err=> {
+				//document.getElementById("responseCode").innerHTML = `An error occurred: ${err.code}`;
+				document.getElementById("response").innerHTML = "sorry geolocation is not supported by your browser or you have denied the permission for us to access your location";
+            },
+
+            {enableHighAccuracy:true, maximumAge: 7000 }
+
+        );
+    }
+    else
+    {
+		document.getElementById("response").innerHTML = "sorry geolocation is not supported by your browser or you have denied the permission for us to access your location";
+    }
+}
