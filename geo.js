f16cace1cc5be3877cdfc9d21bb8855070cfb0fe
Rob <greshr@gmail.com>
2020-02-01 16:00:34 +0000

    enter

--- geo.js
+++ geo.js
@@ -1,11 +1,12 @@
 
 //variables
+var teamColour = "";
 var corner1 = L.latLng(0, 0),
 corner2 = L.latLng(0, 0),
 bounds = L.latLngBounds(corner1, corner2);
 corner1 = map.local({watch:true});
 // create an orange rectangle FIXME: change colour variables
-L.rectangle(bounds, {color: "#ff7800", weight: 1}).addTo(map);
+L.rectangle(bounds, {color: teamColour, weight: 1}).addTo(map);
 // zoom the map to the rectangle bounds
 //map.fitBounds(bounds);
 
